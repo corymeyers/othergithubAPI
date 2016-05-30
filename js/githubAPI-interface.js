e78554e3e05bc7f6dab823c2b03189c238c0cf9f
cory <whippoorwill.in.flight@gmail.com>
2016-05-30 13:15:04 -0700

    add code to return login name from return orject

--- js/githubAPI-interface.js
+++ js/githubAPI-interface.js
@@ -3,10 +3,11 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function() {
   $('#submit').click(function() {
     var username = $('#username').val();
-  
+
     // exports.getRepos = function(){
       $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-        $('.showUser').text(response);
+        
+        $('.showUser').text(response.login);
 
         console.log(response);
       }).fail(function(error){
