3fdf3af0b6a9371ce14068cc0064efc82af271ed
cory <whippoorwill.in.flight@gmail.com>
2016-05-30 11:21:22 -0700

    redacted username.val redundancy

--- js/githubAPI-interface.js
+++ js/githubAPI-interface.js
@@ -3,8 +3,8 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function() {
   $('#submit').click(function() {
     var username = $('#username').val();
-    $('#username').val("");
     $('.showUser').text("The user you have chosen is " + username + ".");
+
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response){
         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
     }).fail(function(error) {
