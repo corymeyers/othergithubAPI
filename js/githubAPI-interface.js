f7888cbe483b4bf4b581a7e5219014cf5fab70f2
cory <whippoorwill.in.flight@gmail.com>
2016-05-30 12:56:56 -0700

    refactored excess code

--- js/githubAPI-interface.js
+++ js/githubAPI-interface.js
@@ -3,21 +3,21 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function() {
   $('#submit').click(function() {
     var username = $('#username').val();
-    $('.showUser').text("The user you have chosen is " + username + ".");
+  
+    // exports.getRepos = function(){
+      $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+        $('.showUser').text(response);
 
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response){
-        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-    $('.showWeather').text(error.responseJSON.message);
-    });
+        console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+    // };
+
+
+
+
+
+
   });
 });
-
-
-// exports.getRepos = function(){
-//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-//     console.log(response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
