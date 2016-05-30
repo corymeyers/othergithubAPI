14ece681e161b96e3cc842d065327e7c8a9d41b8
cory <whippoorwill.in.flight@gmail.com>
2016-05-30 14:07:10 -0700

    add js to find user and display username's repository array

--- js/githubAPI-interface.js
+++ js/githubAPI-interface.js
@@ -4,17 +4,19 @@ $(document).ready(function() {
   $('#submit').click(function() {
     var username = $('#username').val();
 
-    // exports.getRepos = function(){
       $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-        
         $('.showUser').text(response.login);
-
         console.log(response);
       }).fail(function(error){
         console.log(error.responseJSON.message);
       });
-    // };
 
+      $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+        $('.showRepos').text(response);
+        console.log(response);
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
 
 
 
