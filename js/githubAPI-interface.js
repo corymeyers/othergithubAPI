9e924003d4c8327cf6caba5609015db802a70a21
cory <whippoorwill.in.flight@gmail.com>
2016-06-02 10:13:05 -0700

    add broken for loop

--- js/githubAPI-interface.js
+++ js/githubAPI-interface.js
@@ -13,15 +13,16 @@ $(document).ready(function() {
 
       $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
         $('.showRepos').text(response);
-        console.log(response);
+
+        for each (Object in response) {
+          var a = response[0].name)
+        }
+
+
+        // console.log(response);
       }).fail(function(error){
         console.log(error.responseJSON.message);
       });
-
-//this code will loop through the array of repositories, and push the name and description of each array into a new object, which is then placed into an array.
-
-//This will loop through that new array, and print out "repositoryName + repositoryDescription" as list items.
-
 
 
   });
